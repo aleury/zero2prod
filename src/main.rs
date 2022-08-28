33afd82315ea81e78245ba963537a4fbef9fc9f4
Adam Eury <euryadam@gmail.com>
2022-08-28 19:15:03 -0400

    Add Dockerfile. Refactor configuration handling.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -13,14 +13,15 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    // Setup database connection pool and tcp listener.
+    // Setup database connection pool.
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address)?;
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres");
+
+    // Create a TcpListener bound to the application address.
+    let listener = TcpListener::bind(configuration.application.address())?;
 
     // Start the app server.
     run(listener, connection_pool)?.await
